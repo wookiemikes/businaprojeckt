194736242786a4633f5339afd1c37636bf538cf8
wookiemikes <mikhaelp32@gmail.com>
2019-10-11 13:19:12 +0800

    Added Home Page and Login

---
+++ lib/pages/busina_user_login.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+
+class UserLogin extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    
+
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor:Colors.transparent,
+          elevation: 0.0,
+          iconTheme: new IconThemeData(color: Color(0xFF18D191))),
+      body: Container(
+        width: double.infinity,
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            new Stack(
+             children: <Widget>[
+                new Container(
+                height: 150.0,
+                width: 150.0,
+                decoration: new BoxDecoration(
+                  borderRadius: new BorderRadius.circular(75.0),
+                  color: Colors.greenAccent,
+                ),
+                child: new Icon(
+                  Icons.directions_bus,
+                  color: Colors.white,
+                  size: 100.0,
+                  ),
+              ),
+             ],
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, bottom: 50.0),
+                  child: new Text(
+                    "BUSina",
+                    style: new TextStyle(fontSize: 35.0),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
+              child: new TextField(
+                decoration: new InputDecoration(labelText: 'Email'),
+              ),
+            ),
+            new SizedBox(
+              height: 15.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
+              child: new TextField(
+                obscureText: true,
+                decoration: new InputDecoration(labelText: 'Password'),
+              ),
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 20.0, right: 5.0, top: 10.0),
+                    child: GestureDetector(
+                      /*onTap: () {
+                        Navigator.push(context, MaterialPageRoute(
+                          builder: (context) => HomePage()
+                        ));
+                      },*/
+                                          child: new Container(
+                            alignment: Alignment.center,
+                            height: 60.0,
+                            decoration: new BoxDecoration(
+                                color: Color(0xFF18D191),
+                                borderRadius: new BorderRadius.circular(9.0)),
+                            child: new Text("Login",
+                                style: new TextStyle(
+                                    fontSize: 20.0, color: Colors.white))),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 10.0, right: 20.0, top: 10.0),
+                    child: new Container(
+                        alignment: Alignment.center,
+                        height: 60.0,
+                        child: new Text("Forgot Password?",
+                            style: new TextStyle(
+                                fontSize: 17.0, color: Color(0xFF18D191)))),
+                  ),
+                )
+              ],
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(bottom:18.0),
+                    child: new Text("Create A New Account ",style: new TextStyle(
+                                fontSize: 17.0, color: Color(0xFF18D191),fontWeight: FontWeight.bold)),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
