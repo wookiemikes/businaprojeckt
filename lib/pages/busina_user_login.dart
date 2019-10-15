3bb18c44e8488fd66e4862fc7354fde91195746b
Efrenray <efrenraypineda_28@yahoo.com>
2019-10-15 23:28:01 +0800

    changed busina_user_booking to busina_user_home

--- lib/pages/busina_user_login.dart
+++ lib/pages/busina_user_login.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:busina/pages/busina_user_signup.dart';
-import 'package:busina/pages/busina_user_booking.dart';
+import 'package:busina/pages/busina_user_home.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
