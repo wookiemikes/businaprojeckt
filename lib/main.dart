c6908ef12007cd633d6d6f61c3d87e3d42e75da6
Efrenray <efrenraypineda_28@yahoo.com>
2019-10-17 20:27:31 +0800

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -25,147 +25,126 @@ class HomePage extends StatelessWidget {
           decoration: BoxDecoration(
             gradient: LinearGradient(
               begin: Alignment.topCenter,
-              end:  Alignment.bottomCenter,
+              end: Alignment.bottomCenter,
               stops: [0.1, 0.5, 0.7, 0.9],
               colors: [
                 Colors.amber[600],
                 Colors.amber[900],
                 Colors.amber[900],
                 Colors.amber[900],
-
               ],
             ),
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Stack(
+            alignment: Alignment.topCenter,
             children: <Widget>[
-              new Stack(
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  new Container(
+                  Container(
                     height: 150.0,
                     width: 150.0,
                     child: Image.asset('lib/assets/icons/bus.png'),
                   ),
+                  Container(
+                    child: Text('BUSINA',
+                        style: TextStyle(
+                          fontFamily: 'Archivo',
+                          fontSize: 50.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        )),
+                  )
                 ],
               ),
-              new Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 10.0, right: 10.0,
+                ),
+                child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8.0, bottom: 50.0),
-                    child: new Text(
-                      "BUSINA",
-                      style: new TextStyle(
-                        fontFamily: 'Archivo',
-                        fontSize: 50.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
+                  OutlineButton(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      style: BorderStyle.solid,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => UserLogin(),
+                          ));
+                    },
+                    child: Stack(
+                      children: <Widget>[
+                        Container(
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Icon(
+                              Icons.email,
+                              color: Colors.white,
+                              size: 20.0,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Sign in with email',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  OutlineButton(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      style: BorderStyle.solid,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => UserLogin(),
+                          ));
+                    },
+                    child: Stack(
+                      children: <Widget>[
+                        Container(
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Icon(
+                              Icons.phone_android,
+                              color: Colors.white,
+                              size: 20.0,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Sign in with phone number',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   )
                 ],
               ),
-              new Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          left: 20.0, right: 20.0, top: 10.0),
-                      child: OutlineButton(
-                        borderSide: BorderSide(
-                          color: Colors.white,
-                          style: BorderStyle.solid,
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => UserLogin(),
-                              ));
-                        },
-                        child: Stack(
-                          children: <Widget>[
-                            Container(
-                              child: Align(
-                                alignment: Alignment.centerLeft,
-                                child: Icon(
-                                  Icons.email,
-                                  color: Colors.white,
-                                  size: 20.0,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              child: Align(
-                                alignment: Alignment.center,
-                                child: Text(
-                                  'Sign in with email',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 15.0,
-                                  ),
-                                  
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-               new Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          left: 20.0, right: 20.0, top: 10.0),
-                      child: OutlineButton(
-                        borderSide: BorderSide(
-                          color: Colors.white,
-                          style: BorderStyle.solid,
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => UserLogin(),
-                              ));
-                        },
-                        child: Stack(
-                          children: <Widget>[
-                            Container(
-                              child: Align(
-                                alignment: Alignment.centerLeft,
-                                child: Icon(
-                                  Icons.phone_android,
-                                  color: Colors.white,
-                                  size: 20.0,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              child: Align(
-                                alignment: Alignment.center,
-                                child: Text(
-                                  'Sign in with phone number',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 15.0,
-                                  ),
-                                  
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              )
             ],
           ),
         ),
